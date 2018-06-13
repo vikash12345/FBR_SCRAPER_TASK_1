7294b1ae8b7d8a4faf7bf2d2a2cde5eff24e1f17
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-06-13 08:46:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -35,7 +35,7 @@ $cHeadres = array(
        }
 //Main Code Start Here.
 //Total Pages in that site is 4995153
-for($page = 40206;$page <1199000; $page++)
+for($page = 50205;$page <1199000; $page++)
  {
 	
 	
